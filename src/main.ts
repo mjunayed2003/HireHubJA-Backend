a204a326b4f95f44ff85141605a1151d44cb8ade
mjunayed2003 <junayedislammd8@gmail.com>
2026-03-14 01:08:21 +0600

    update json

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,7 @@ async function bootstrap() {
     prefix: '/uploads',
   });
 
-  await app.listen(process.env.PORT ?? 4000);
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
 }
 bootstrap();
